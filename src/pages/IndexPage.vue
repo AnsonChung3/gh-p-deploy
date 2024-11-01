486d3a3bfcc7f7de295f5a952a017e58a78a3352
Anson Chung <ansonchung3@gmail.com>
2024-11-01 16:13:57 +0000

    Changed page content from quasar logo to 'Anson' to make visible difference if page would deploy

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,11 @@
 <template>
   <q-page class="flex flex-center">
-    <img
+    <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    > -->
+    <h1>Anson</h1>
   </q-page>
 </template>
 
